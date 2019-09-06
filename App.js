768599996b5f09bc033f5db85deb973f8d4f0580
lauragift21 <laurandidi21@gmail.com>
2019-09-06 20:29:17 +0100

    Add flatlist and data from 3rd party API

--- App.js
+++ App.js
@@ -1,13 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import unsplash from './config';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import { Image } from 'react-native-elements';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      client_id: '3ab52700b5843c4aa63c29db436b8d854cdcbd9e8917a6fb378a22c7ddda07f6',
+    }
+  }
+
+  componentDidMount() {
+    this.makeRequest();
+  }
+
+  makeRequest = () => {
+    const { client_id } = this.state;
+  const url = `https://api.unsplash.com/photos?client_id=${client_id}&page=1`;
+    return fetch(url).then((response) => {
+      response.json()
+      console.log(response);
+      this.setState({
+        isLoading: false,
+        data: [response]
+      })
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  }
+
+  renderLoading = () => {
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Resplash App</Text>
-        <Text>A clone of Unsplash don't take it seriously</Text>
+        <Text>A clone of Unsplash don't take it too seriously 😃</Text>
+        <FlatList
+          data={data}
+          renderItem={({item, index}) => (
+            <Image
+              source={{uri: item.urls }}
+              style={{ width: 100, height: 100}}
+              PlaceholderContent={<ActivityIndicator />}
+            />
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
       </View>
     );
   }
@@ -18,9 +68,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   text: {
-    fontSize: 23,
+    fontSize: 33,
+  },
+  item: {
+    fontSize: 34,
   }
 });
